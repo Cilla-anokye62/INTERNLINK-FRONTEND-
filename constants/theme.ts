307b59128a51f40afabb18e5a27cf844ec2ec728
Cilla-anokye62 <anokyepriscilla62@gmail.com>
2026-06-10 10:21:20 +0000

    initial setup: InternLink frontend

---
+++ constants/theme.ts
@@ -0,0 +1,23 @@
+export const LightColors = {
+  text: '#050315',
+  background: '#D9F5F0',
+  primary: '#2CACAD',
+  secondary: '#024D60',
+  accent: '#75E2E0',
+};
+
+export const DarkColors = {
+  text: '#EBE9FC',
+  background: '#02364A',
+  primary: '#024D60',
+  secondary: '#2CACAD',
+  accent: '#1D8B89',
+};
+
+export const FontSizes = {
+  xs: 12, sm: 14, md: 16, lg: 20, xl: 24, xxl: 32,
+};
+
+export const Spacing = {
+  xs: 4, sm: 8, md: 16, lg: 24, xl: 32,
+};
